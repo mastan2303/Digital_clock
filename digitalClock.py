ed611d36771e282f553ebbca442c14055597dd72
mastan2303 <mastan.paluri@sarasanalytics.com>
2023-06-23 15:59:29 +0530

    removed welcome message

--- digitalClock.py
+++ digitalClock.py
@@ -6,7 +6,7 @@ window.geometry("600x300")
 window.configure(bg = "steelblue")
 
 
-label = Label(window, text = 'Welcome!', font = ("Ariel Black",78,"bold"), bg = "steelblue", fg = "white")
+label = Label(window, font = ("Ariel Black",78,"bold"), bg = "steelblue", fg = "white")
 label.pack(pady = 100)
 
 
